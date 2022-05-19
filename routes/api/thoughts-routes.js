9650439d9c653451370fff6e19f6cdbcc5cd3a80
Nate-Huerd <nhuerd3@gmail.com>
2022-05-19 12:25:31 -0500

    finished thoughts route

--- routes/api/thoughts-routes.js
+++ routes/api/thoughts-routes.js
@@ -4,8 +4,9 @@ const {
     getThoughts,
     getSingleThought,
     addReaction,
-    removeReaction,
+    deleteReaction,
     createThought,
+    updateThought,
     deleteThought,
 } = require('../../controllers/thoughts-controller');
 
@@ -13,4 +14,13 @@ const {
 router.route('/').get(getThoughts);
 router.route('/').post(createThought);
 
-router.route
+// singular thought Id
+router.route('/:thoughtId').get(getSingleThought);
+router.route('/:thoughtId').put(updateThought);
+router.route('/:thoughtId').delete(deleteThought);
+
+// reactions
+router.route('/:thoughtId/reactions').post(addReaction);
+router.route('/:thoughtId/reactions/reactionId').delete(deleteReaction);
+
+module.exports = router;
