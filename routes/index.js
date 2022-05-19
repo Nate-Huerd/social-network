bb7dede637adb8411a291bd34c557f9ffdbfd461
Nate-Huerd <nhuerd3@gmail.com>
2022-05-19 11:56:00 -0500

    removed router.use for html

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,9 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
-router.use('/', htmlRoutes);
 
 router.use((req, res) => {
-  res.status(404).send('<h1>404 Error!</h1>');
+  return res.send('404 Error!');
 });
 
 module.exports = router;
